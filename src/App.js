325ce53e68332ff88dda676a2f629728f47466b9
leunghochun <leunghochun@gmail.com>
2024-01-05 16:49:27 +0000

    init checkin

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+import Speech from './Speech';
+function App() {
+    return (
+        <div className="App">
+            <Speech />
+        </div>
+    );
+}
+export default App;
