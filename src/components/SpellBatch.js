d14ca2b521e72acdfac48a1a84797d175112d57b
leunghochun <41934157+leunghochun@users.noreply.github.com>
2024-01-17 14:19:31 +0000

    progress bar change

--- src/components/SpellBatch.js
+++ src/components/SpellBatch.js
@@ -10,24 +10,10 @@ const SpellBatch = (props) => {
     const [ started, setStarted ] = React.useState(false);
     const [ timers, setTimers ] = React.useState([]);
     const [ progressTimer, setProgressTimer ] = React.useState();
+    const [ progressBarColor, setProgressBarColor] = React.useState("progress-bar-green");
     const [ countOfProgess, setCountOfProgess ] = React.useState(0);
     const [ counter, setCounter ] = React.useState(0);
     const speakSleep = 3000;
-
-    // const timer = setInterval(() => {
-    //     setCountOfProgess((oldProgress) => {
-    //         console.log("timer");
-    //         if (!started) return 0;
-    //         if (counter === props.settings.waitTime) {
-    //             setCounter(-1);
-    //             setIndex((prevIndex)=> prevIndex + 0);
-    //         } else {
-    //             setCounter((counter) => counter + 1);
-    //         }
-    //         console.log("counter:", counter, ",", counter / props.settings.waitTime * 99);
-    //         return counter/ props.settings.waitTime * 99;
-    //     });
-    // }, 999);
 
     const speakWord = (index) => {
         for(let j = 0; j < props.settings.repeatTime; j++) {
@@ -66,10 +52,15 @@ const SpellBatch = (props) => {
             if (counter === props.settings.waitTime) {
                 setCounter(-1);
                 setIndex((prevIndex)=> prevIndex + 1);
+                setProgressBarColor("progress-bar-green");
             } else {
                 setCounter((counter) => counter + 1);
             }
-            console.log("counter:", counter, ",", counter / props.settings.waitTime * 100);
+            let progress = counter/ props.settings.waitTime * 100;
+            if (progress >= 50) {
+                setProgressBarColor(progress >= 75 ? "progress-bar-red" : "progress-bar-yellow");
+            }
+            console.log("counter:", counter, ",", progress);
             return counter/ props.settings.waitTime * 100;
         });
     };
@@ -88,7 +79,7 @@ const SpellBatch = (props) => {
             {   
                 started ?
                     <>
-                        <ProgressBar now={countOfProgess} />
+                        <ProgressBar className={progressBarColor} now={countOfProgess} />
                         <SpellTest word={props.words[index]} voice={props.voice} settings={props.settings} setCorrectWord={props.setCorrectWord}/>
                         <Button className="button bg-info"  onClick={() => {startClicked(false)}}>Stop</Button>
                         <Button className="button bg-info"  onClick={() => setIndex((prevIndex) => prevIndex + 1)}>Next</Button> 
