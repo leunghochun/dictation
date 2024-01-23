da3d853937f2795547f0b32712e9cb4793c2c493
leunghochun <41934157+leunghochun@users.noreply.github.com>
2024-01-23 10:56:56 +0000

    test input box handling change

--- src/components/SpellTest.js
+++ src/components/SpellTest.js
@@ -2,31 +2,42 @@ import React from "react";
 import Form from 'react-bootstrap/Form'
 
 const SpellTest = (props) => {
+    const [ key, setKey] = React.useState("");
     const [ word, setWord] = React.useState("");
     const [ isCorrect, setIsCorrect ] = React.useState(false);
 
     const inputKeyUp = (event) =>{
-        let text = event.target.value;
+        console.log(event.target.value);
+        if (event.keyCode === 8) {
+            setWord((word) => word.substring(0, word.length-1));
+            return;
+        }
+        let char = event.target.value;
+        let text = word + char;
+        console.log(char, text);
         let correctChars = [];
-        let isCorrect = true;
-        setWord(text);
+        let correct = true;
         for (let i = 0; i < text.length; i++) {
             if (text[i].toLowerCase() === props.word[i].toLowerCase())
                 correctChars.push(text[i]);
             else {
-                isCorrect = false;
+                correct = false;
                 break;
             }
         }
-        setIsCorrect(isCorrect && text.length === props.word.length);
-    }
+        console.log("correct:", correct, correct && text.length === props.word.length);
+        setIsCorrect(correct && text.length === props.word.length);
+        setWord(text);
+        setKey("");
+    };
 
     React.useEffect(()=> {
         if (props.word) {
             props.setCorrectWord(props.word, isCorrect);
             setWord("");
         }
-    },[isCorrect]);
+        console.log("isCorrect Effect:", isCorrect);
+    }, [isCorrect]);
 
     React.useEffect(() => {
         setWord("");
@@ -36,7 +47,8 @@ const SpellTest = (props) => {
     return (
         <>
             {isCorrect ? props.word : ""}
-            <Form.Control size="lg" type="text" placeholder="Vocabulary" onKeyUp={inputKeyUp} onChange={inputKeyUp} value={word}/>
+            <Form.Label className="word-tested word" key={word}>{word}</Form.Label> 
+            <Form.Control size="lg" type="text" placeholder="Vocabulary" onKeyUp={inputKeyUp} onChange={inputKeyUp} value={key}/>
         </>
     );
 };
